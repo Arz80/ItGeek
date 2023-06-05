9b07525ca132e3bf1b7ee438895a4ac43bd441d4
Arz80 <1980sam2006@gmail.com>
2023-06-05 19:07:45 +0600

    Добавили Tegs

--- src/ItGeek.BLL/Repositories/TagRepository.cs
+++ src/ItGeek.BLL/Repositories/TagRepository.cs
@@ -1,6 +1,7 @@
 ﻿using ItGeek.DAL.Data;
 using ItGeek.DAL.Entities;
 using ItGeek.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,15 @@ namespace ItGeek.BLL.Repositories;
 
 public class TagRepository : GenericRepositoryAsync<Tag>, ITagRepository
 {
+    private readonly AppDbContext _db;
+
     public TagRepository(AppDbContext db) : base(db)
     {
+        _db = db;
+    }
+
+    public async Task<List<Tag>> GetTagByNameAsync(string tagName)
+    {
+        return await _db.Tags.Where(x => x.Name.Contains(tagName)).ToListAsync();
     }
 }
