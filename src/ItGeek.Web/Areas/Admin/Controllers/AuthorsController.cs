72a95ef2dbb759cb6fa6b68a0d7331560cddfd0e
Arz80 <1980sam2006@gmail.com>
2023-06-24 12:57:46 +0600

    Доработка

--- src/ItGeek.Web/Areas/Admin/Controllers/AuthorsController.cs
+++ src/ItGeek.Web/Areas/Admin/Controllers/AuthorsController.cs
@@ -1,6 +1,8 @@
 ﻿using ItGeek.BLL;
 using ItGeek.DAL.Entities;
+using ItGeek.Web.Areas.Admin.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using System.Security.Policy;
 
 namespace ItGeek.Web.Areas.Admin.Controllers;
@@ -9,10 +11,12 @@ namespace ItGeek.Web.Areas.Admin.Controllers;
 public class AuthorsController : Controller
 {
     private readonly UnitOfWork _uow;
+    private readonly IWebHostEnvironment _hostEnvironment;
 
-    public AuthorsController(UnitOfWork uow)
+    public AuthorsController(UnitOfWork uow, IWebHostEnvironment hostEnvironment)
     {
         _uow = uow;
+        _hostEnvironment = hostEnvironment;
     }
 
     public async Task<IActionResult> Index()
@@ -39,6 +43,7 @@ public class AuthorsController : Controller
     [HttpGet]
     public async Task<IActionResult> Create()
     {
+        ViewBag.isCreate = true;
         return View();
     }
     [HttpPost]
@@ -46,9 +51,12 @@ public class AuthorsController : Controller
     {
         if (ModelState.IsValid)
         {
+            
+            author.AuthorImage = await ProcessUploadFile(author);
             await _uow.AuthorRepository.InsertAsync(author);
             return RedirectToAction(nameof(Index));
         }
+        ViewBag.isCreate = true;
 
         return View(author);
     }
@@ -60,7 +68,7 @@ public class AuthorsController : Controller
         {
             return NotFound();
         }
-
+        ViewBag.isCreate = false;
         return View(author);
     }
     [HttpPost]
@@ -68,11 +76,36 @@ public class AuthorsController : Controller
     {
         if (ModelState.IsValid)
         {
+            // Получили новую картинку 
+            if (author.ImageFile != null)
+            {
+                string newImage = await ProcessUploadFile(author);
+                author.AuthorImage = newImage;
+            }
+
             await _uow.AuthorRepository.UpdateAsync(author);
             return RedirectToAction(nameof(Index));
         }
-
+        ViewBag.isCreate = false;
         return View(author);
+    }
+
+    protected async Task<string> ProcessUploadFile(Author author)
+    {
+        string uniqueFileName = "";
+        if (author.ImageFile != null)
+        {
+            string wwwRootPath = _hostEnvironment.WebRootPath;
+            string fileName = Path.GetFileNameWithoutExtension(author.ImageFile.FileName);
+            string fileExtension = Path.GetExtension(author.ImageFile.FileName);
+            uniqueFileName = fileName + DateTime.Now.ToString("yymmddssfff") + fileExtension;
+            string path = Path.Combine(wwwRootPath + "/uploads/", uniqueFileName);
+            using (var fileStream = new FileStream(path, FileMode.Create))
+            {
+                await author.ImageFile.CopyToAsync(fileStream);
+            }
+        }
+        return uniqueFileName;
     }
 
 }
